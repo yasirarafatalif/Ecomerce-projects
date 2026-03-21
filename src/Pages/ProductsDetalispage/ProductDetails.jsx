d19c88d462bdd52100bafb1539d91d310a667a6c
yasirarafatalif <yasirarafatalif1@gmail.com>
2026-03-21 13:46:21 +0600

    order placed fucntion add

--- src/Pages/ProductsDetalispage/ProductDetails.jsx
+++ src/Pages/ProductsDetalispage/ProductDetails.jsx
@@ -14,62 +14,91 @@ import useAxios from "../../Hooks/useAxios";
 import { HiShoppingBag } from "react-icons/hi";
 import { GoPackage } from "react-icons/go";
 import PremiumLoader from "../../Components/Shared/PremiumSpinner";
+import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const ProductDetails = () => {
-  const {id} = useParams()
-  const axois= useAxios()
+  const { id } = useParams();
+  const axois = useAxios();
   const [active, setActive] = useState(null);
 
+  const { user } = useAuth();
+  const { data: products = [], isLoading } = useQuery({
+    queryKey: ["products-detalis", id],
+    queryFn: async () => {
+      const res = await axois.get(`/products/${id}`);
+      return res.data;
+    },
+  });
 
-
-  const {data : products=[] ,isLoading}= useQuery({
-    queryKey:["products-detalis", id],
-    queryFn: async()=>{
-      const res= await  axois.get(`/products/${id}`)
-      return res.data
-
-    }
-  })
-
-  console.log(products)
-
-
-    const detailsData = [
-  {
-    title: "Description",
-    content: `${products?.description}`,
-  },
-  {
-    title: "Care Instructions",
-    content: "Wash in cold water. Do not bleach. Iron at low temperature.",
-  },
-  {
-    title: "Sustainability",
-    content: "Made using eco-friendly materials and sustainable process.",
-  },
-];
+  const detailsData = [
+    {
+      title: "Description",
+      content: `${products?.description}`,
+    },
+    {
+      title: "Care Instructions",
+      content: "Wash in cold water. Do not bleach. Iron at low temperature.",
+    },
+    {
+      title: "Sustainability",
+      content: "Made using eco-friendly materials and sustainable process.",
+    },
+  ];
   // Product Images Array
 
-  
-
-
   const productImages = [
-
     "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?q=80&w=800",
-     `${products?.img}`,
+    `${products?.img}`,
   ];
 
   // States
   const [mainImage, setMainImage] = useState(productImages[3]);
-  const [selectedSize, setSelectedSize] = useState("M");
+  const [selectedSize, setSelectedSize] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   const sizes = ["XS", "S", "M", "L", "XL", "2X"];
   useEffect(() => {
-  if (products?.img) {
-    setMainImage(products.img);
-  }
-}, [products]);
+    if (products?.img) {
+      setMainImage(products.img);
+    }
+  }, [products]);
+
+  const hanadelAddToCart = async (product) => {
+    const cartData = {
+      productName: product?.title,
+      productCategory: product?.category,
+      buyerEmail: user?.email,
+      productId: product?._id,
+      status: "Pending",
+      deliveryStatus: "Pending",
+      paymentStatus: "Unpaid",
+      totalQuantity: quantity,
+      productPrice: product?.price,
+      productType: product?.gender,
+      size: selectedSize,
+    };
+    const res = await axois.post("/orders", cartData);
+    console.log(res);
+    if (res.data.success) {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: `${res.data.message}`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else{
+       Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: `${res.data.message}`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+    
+  };
 
   if (isLoading) {
     return <PremiumLoader></PremiumLoader>;
@@ -77,7 +106,7 @@ const ProductDetails = () => {
 
   return (
     <div className="min-h-screen bg-[#f2f2f2] pt-28 pb-20 font-sans">
-      <title>Product Details</title>
+      <title>Product Details {products.title}</title>
       <div className="max-w-[1440px] mx-auto px-4 md:px-12">
         <div className="flex flex-col lg:flex-row gap-12 items-start">
           {/* --- LEFT: IMAGE SECTION --- */}
@@ -121,17 +150,15 @@ const ProductDetails = () => {
                 <p className="text-[10px] font-black uppercase tracking-[0.2em] text-gray-400 mb-1 italic">
                   XIV Exclusive
                 </p>
-                
+
                 <h1 className="text-4xl font-black uppercase tracking-tighter italic text-gray-900 leading-tight">
                   {products?.title}
                 </h1>
 
                 <div className=" ">
-
-                   <span className="text-[10px] font-bold uppercase  text-gray-500">
-                     CATEGORY: {products?.category}
-                    </span>
-                  
+                  <span className="text-[10px] font-bold uppercase  text-gray-500">
+                    CATEGORY: {products?.category}
+                  </span>
                 </div>
                 <div className="flex items-center gap-4 mt-4">
                   <span className="text-2xl font-black text-gray-900">
@@ -141,13 +168,12 @@ const ProductDetails = () => {
                   <div className="flex items-center gap-1">
                     <Star size={14} className="fill-black" />
                     <span className="text-[10px] font-bold uppercase tracking-widest text-gray-500">
-                      {products?.rating || "4.0"} ({products?.reviewsCount || 0})
+                      {products?.rating || "4.0"} ({products?.reviewsCount || 0}
+                      )
                     </span>
                   </div>
-                  
                 </div>
                 <div className="flex mt-3 gap-3">
-
                   <div className="flex items-center gap-1">
                     <GoPackage size={14} className="fill-black" />
                     <span className="text-[10px] font-bold uppercase tracking-widest text-gray-500">
@@ -161,8 +187,6 @@ const ProductDetails = () => {
                     </span>
                   </div>
                 </div>
-
-                
               </div>
 
               {/* Size Selector */}
@@ -171,12 +195,12 @@ const ProductDetails = () => {
                   Select Size
                 </p>
                 <div className="grid grid-cols-6 gap-2">
-                  {products?.inventory?.map((size) => (
+                  {products?.inventory?.map((size, i) => (
                     <button
-                      key={size}
-                      onClick={() => setSelectedSize(size)}
+                      key={i}
+                      onClick={() => setSelectedSize(size.size)}
                       className={`py-3 text-[11px] font-bold border transition-all
-                        ${selectedSize === size ? "bg-black text-white border-black shadow-lg" : "bg-white border-gray-200 text-gray-400 hover:border-black hover:text-black"}`}
+                        ${selectedSize === size.size ? "bg-black text-white border-black shadow-lg" : "bg-white border-gray-200 text-gray-400 hover:border-black hover:text-black"}`}
                     >
                       {size.size}
                     </button>
@@ -202,53 +226,54 @@ const ProductDetails = () => {
                   </button>
                 </div>
 
-                <Link to={"/checkout"}>
-                <button className="w-full bg-[#1A1A1A] text-white py-5 flex items-center justify-center gap-4 hover:bg-black transition-all group active:scale-[0.98] shadow-xl">
-                  <ShoppingBag
-                    size={20}
-                    className="group-hover:rotate-12 transition-transform"
-                  />
-                  
-                  <span className="text-sm font-black uppercase tracking-[0.25em]">
-                    Add to Bag
-                  </span>
-                </button>
-                
-                </Link>
+                <Link
+                //  to={"/checkout"}
+                >
+                  <button
+                    onClick={() => hanadelAddToCart(products)}
+                    className="w-full bg-[#1A1A1A] text-white py-5 flex items-center justify-center gap-4 hover:bg-black transition-all group active:scale-[0.98] shadow-xl"
+                  >
+                    <ShoppingBag
+                      size={20}
+                      className="group-hover:rotate-12 transition-transform"
+                    />
 
-                
+                    <span className="text-sm font-black uppercase tracking-[0.25em]">
+                      Add to Bag
+                    </span>
+                  </button>
+                </Link>
               </div>
 
               {/* Details Toggles */}
               <div className="border-t border-gray-200 pt-2">
-  {detailsData.map((item, index) => (
-    <div key={item.title} className="border-b border-gray-200">
-      
-      {/* Header */}
-      <div
-        onClick={() => setActive(active === index ? null : index)}
-        className="flex justify-between items-center py-4 cursor-pointer hover:px-2 transition-all"
-      >
-        <span className="text-[11px] font-black uppercase tracking-widest text-gray-700">
-          {item.title}
-        </span>
-        <ChevronDown
-          size={16}
-          className={`transition-transform ${
-            active === index ? "rotate-180" : ""
-          }`}
-        />
-      </div>
+                {detailsData.map((item, index) => (
+                  <div key={item.title} className="border-b border-gray-200">
+                    {/* Header */}
+                    <div
+                      onClick={() => setActive(active === index ? null : index)}
+                      className="flex justify-between items-center py-4 cursor-pointer hover:px-2 transition-all"
+                    >
+                      <span className="text-[11px] font-black uppercase tracking-widest text-gray-700">
+                        {item.title}
+                      </span>
+                      <ChevronDown
+                        size={16}
+                        className={`transition-transform ${
+                          active === index ? "rotate-180" : ""
+                        }`}
+                      />
+                    </div>
 
-      {/* Dropdown Content */}
-      {active === index && (
-        <div className="pb-4 text-[12px] text-gray-500">
-          {item.content}
-        </div>
-      )}
-    </div>
-  ))}
-</div>
+                    {/* Dropdown Content */}
+                    {active === index && (
+                      <div className="pb-4 text-[12px] text-gray-500">
+                        {item.content}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
 
               {/* Social Share */}
               <div className="flex items-center gap-4 opacity-50 hover:opacity-100 transition-opacity cursor-pointer">
